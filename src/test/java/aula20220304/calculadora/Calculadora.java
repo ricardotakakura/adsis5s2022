d539e2d3f50cb59fd379a2456b8d3da5479152ee
arthur.zavadski <aczavads@gmail.com>
2022-03-04 20:16:50 -0300

    Implementação dos testes com calculadora.

---
+++ src/test/java/aula20220304/calculadora/Calculadora.java
@@ -0,0 +1,27 @@
+package aula20220304.calculadora;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Calculadora {
+
+	public double somar(double v1, double v2) {
+		//return 20.25;
+		return v1 + v2;
+	}
+
+	public double subtrair(double v1, double v2) {
+		return v1 - v2;
+	}
+
+	public double dividir(double v1, double v2) {
+		BigDecimal aux = new BigDecimal(v1 / v2);
+		aux = aux.setScale(6, RoundingMode.DOWN);
+		return aux.doubleValue();
+	}
+
+	public double multiplicar(double v1, double v2) {
+		return v1 * v2;
+	}
+
+}
---
+++ src/test/java/aula20220304/calculadora/TestesIntrodutórios.java
@@ -0,0 +1,54 @@
+package aula20220304.calculadora;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestesIntrodutórios {
+
+	@Test
+	public void teste_de_soma() {
+		Calculadora calc = new Calculadora();
+		double total = calc.somar(10, 10.25);		
+		assertEquals(20.25, total, 0);
+		
+		total = calc.somar(15, 77.10);		
+		assertEquals(92.10, total, 0);
+
+	}
+	@Test
+	public void teste_de_subtração() {
+		Calculadora calc = new Calculadora();
+		double total = calc.subtrair(10, 10.25);		
+		assertEquals(-0.25, total, 0);
+		
+		total = calc.subtrair(77, 15);		
+		assertEquals(62, total, 0);
+
+	}
+	@Test
+	public void teste_de_divisão() {
+		Calculadora calc = new Calculadora();
+		double total = calc.dividir(10, 2);		
+		assertEquals(5, total, 0);
+		
+		total = calc.dividir(15, 3);		
+		assertEquals(5, total, 0);
+		
+		total = calc.dividir(16, 3.3);		
+		assertEquals(4.848484, total, 0);
+
+
+	}
+	@Test
+	public void teste_de_multiplicação() {
+		Calculadora calc = new Calculadora();
+		double total = calc.multiplicar(10, 3.5);		
+		assertEquals(35, total, 0);
+		
+		total = calc.multiplicar(15, 3);		
+		assertEquals(45, total, 0);
+
+	}
+
+}
